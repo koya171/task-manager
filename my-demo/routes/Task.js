71d66db5d482e82a215e3935812caffd676a84fa
koya171 <narendrakoya09@gmail>
2022-12-11 14:48:59 +0530

    my final commit

---
+++ my-demo/routes/Task.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router();
+const {
+    getAlltasks,
+    Createtask,
+    gettask,
+    Updatetask,
+    Deletetask
+} = require('../controolers/tsak')
+
+router.route('/').get(getAlltasks).post(Createtask);
+router.route('/:id').get(gettask).patch(Updatetask).delete(Deletetask);
+
+module.exports=router
